887f930b1c03c1dcc4daea37bced69f7a18d5cd5
vanight <712220+vanight@users.noreply.github.com>
2021-02-25 22:55:55 -0800

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Scraper } from './service/scraper';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   imports: [CacheModule.register({
     ttl: 60, // seconds
     max: 10, // maximum number of items in cache
-  })
-  ],
+  }),
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:'
+  }),],
   controllers: [AppController],
   providers: [AppService, Scraper, BitcoinService,
     {
